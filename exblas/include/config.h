43ae29b819591ba6db2f29b10e655f8214333b48
Victor Nicolet <victor.nicolet@polytechnique.edu>
2017-11-28 14:05:12 -0500

    Moving code

--- exblas/include/config.h
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2016 Inria and University Pierre and Marie Curie
-// All rights reserved.
-
-// the configured options and settings for ExBLAS
-#define EXBLAS_VERSION_MAJOR 1
-#define EXBLAS_VERSION_MINOR 0
-#define EXBLAS_SOURCE_DIR "/home/nicolet/projects/precise_parallel_fp/exblas"
-#define EXBLAS_BINARY_DIR "/home/nicolet/projects/precise_parallel_fp/exblas"
-/* #undef USE_EXBLAS */
