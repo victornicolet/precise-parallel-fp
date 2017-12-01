b9b2c51a65317c70319bc4e43a36610cbfbb1f1c
Victor Nicolet <victor.nicolet@polytechnique.edu>
2017-12-01 12:15:38 -0500

    Revert "Clean up cmake generated files."
    This reverts commit 377ff1634f2771d74ddb5289cb480cad58571ecc.

---
+++ capps/exblas/src/cpu/blas1/CMakeFiles/test.exsum.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nicolet/projects/precise_parallel_fp/exblas/tests/test.exsum.cpu.cpp" "/home/nicolet/projects/precise_parallel_fp/exblas/src/cpu/blas1/CMakeFiles/test.exsum.dir/__/__/__/tests/test.exsum.cpu.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "src/common"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/nicolet/projects/precise_parallel_fp/exblas/src/cpu/CMakeFiles/exblas.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ capps/exblas/src/cpu/blas1/CMakeFiles/test.exsum.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test.exsum.dir/__/__/__/tests/test.exsum.cpu.cpp.o"
+  "test.exsum.pdb"
+  "test.exsum"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test.exsum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
