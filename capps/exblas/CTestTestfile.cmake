d2053709e42c581bbc3f9ec2f908acdc00a56c06
Victor Nicolet <victor.nicolet@polytechnique.edu>
2017-12-01 12:19:25 -0500

    Clean up again

--- capps/exblas/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nicolet/projects/precise_parallel_fp/exblas
-# Build directory: /home/nicolet/projects/precise_parallel_fp/exblas
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(src)
