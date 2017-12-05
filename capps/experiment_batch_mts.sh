53c915afdeaacfe16116c0c15248a0d774596fe4
Victor Nicolet <victor.nicolet@polytechnique.edu>
2017-12-05 14:02:20 -0500

    No need for the small values

--- capps/experiment_batch_mts.sh
+++ capps/experiment_batch_mts.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for i in 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29
+for i in 20 21 22 23 24 25 26 27 28 29
 do
 echo "Experiment for $i"
 ./precise_parallel_fp $i
