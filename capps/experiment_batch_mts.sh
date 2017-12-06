2e14f4acb091bafbc24ba6ee847b3820fb15d769
Victor Nicolet <victor.nicolet@polytechnique.edu>
2017-12-06 17:10:14 -0500

    Update script

--- capps/experiment_batch_mts.sh
+++ capps/experiment_batch_mts.sh
@@ -2,6 +2,8 @@
 
 for i in 20 21 22 23 24 25 26 27 28 29 30
 do
-echo "Experiment for $i"
-./precise_parallel_fp $i
+echo "MTS experiment for $i ------------------------"
+./precise_parallel_fp 0 $i
+echo "POLY experiment for $i ------------------------"
+./precise_parallel_fp 1 $i
 done
