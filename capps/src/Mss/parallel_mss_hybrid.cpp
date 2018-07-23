9ae9eafd13c5427579e27abe176a3687c9256360
rdangnhu on toe <raphael.dangnhu@gmail.com>
2018-07-23 11:18:58 -0400

    Continue hybrid mss

--- capps/src/Mss/parallel_mss_hybrid.cpp
+++ capps/src/Mss/parallel_mss_hybrid.cpp
@@ -2,7 +2,24 @@
 
 #include <iostream>
 
+#include "tbb/task_group.h"
+#include "tbb/parallel_reduce.h"
+
+#include "parallel_mss.hpp"
+
+using namespace tbb;
 using namespace std;
+
+struct mss_struct {
+    double sum;
+    double mss;
+    double mts;
+    double mps;
+    long posl;
+    long posr;
+    long posMps;
+    long posMts;
+};
 
 class HybridMssReduction : public task {
     public :
@@ -18,20 +35,69 @@ class HybridMssReduction : public task {
     task* execute(){
         if(depth == 0){
             // Call parallel_reduce
-            res->mss = mss;
-            res->mts = mts;
-            res->pos = pos;
-            return NULL;
+            __mss_naive result = __mss_naive(a);
+            parallel_reduce(blocked_range<long>(left,right),result);
+            res -> sum = result.sum;
+            res -> mss = result.mss;
+            res -> mts = result.mts;
+            res -> mps = result.mps;
+            res -> posl = result.posmssl;
+            res -> posr = result.posmssr;
+            res -> posMps = result.posmps;
+            res -> posMts = result.posmts;
+
         }
         else{
-            // Call subtasks and return result;
-                MpsTask1& lTask = *new(c.allocate_child()) MpsTask1(Cutoff,array,sizel,sum,mps,position,left,middle);
-                MpsTask1 &rTask = *new(c.allocate_child()) MpsTask1(depth,array,&c.rres,middle,right);
+            int newDepth = depth - 1;
+            mss_struct resLeft;
+            mss_struct resRight;
+            long middle = (left + right) / 2;
 
-                c.set_ref_count(3);
+            // Call subtasks and return result;
+                HybridMssReduction& lTask = *new(allocate_child()) HybridMssReduction(newDepth,a,&resLeft,left,middle);
+                HybridMssReduction &rTask = *new(allocate_child()) HybridMssReduction(newDepth,a,&resRight,middle,right);
+
+                set_ref_count(3);
+                spawn(lTask);
+                spawn_and_wait_for_all(rTask);
+
+                // Join
+                // Sum
+                res -> sum = resLeft . sum + resRight . sum;
+
+                // Mps
+                double mpsAux = resLeft . sum + resRight . mps;
+                if(mpsAux >= resLeft.mps){
+                    res -> mps = mpsAux;
+                    res -> posMps = resRight.posMps;
+                }else{
+                    res -> mps = resLeft.mps;
+                    res -> posMps = resLeft.posMps;
+                }
+
+                // Mts
+                double mtsAux = resRight.sum + resLeft.mts;
+                if(mtsAux >= resRight.mts){
+                    res -> mps = mtsAux;
+                    res -> posMts = resLeft.posMts;
+                }else{
+                    res->mts = resRight.mts;
+                    res->posMts = resRight.posMts;
+                }
+
+                // Mss
+
         }
+        return NULL;
     }
-}
+    private:
+        int depth;
+        double* a;
+        mss_struct* res;
+        unsigned long left;
+        unsigned long right;
+        boolean** memo;
+};
 
 
 void parallel_mps_interval_hybrid(){
