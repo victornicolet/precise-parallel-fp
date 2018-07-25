78eea1ebd52a884f94385769b0741be38affe377
rdangnhu on toe <raphael.dangnhu@gmail.com>
2018-07-25 09:14:33 -0400

    Added variable dep analysis

--- capps/src/Mss/parallel_mss_hybrid.cpp
+++ capps/src/Mss/parallel_mss_hybrid.cpp
@@ -147,6 +147,16 @@ void parallel_mss_hybrid(double* a, long size){
 }
 
 struct mss_struct_bool {
+    mss_struct_bool() :
+        sum(false),
+        mss(false),
+        mts(false),
+        mps(false),
+        posl(false),
+        posr(false),
+        posMps(false),
+        posMts(false)
+    {}
     bool sum;
     bool mss;
     bool mts;
@@ -341,92 +351,225 @@ class HybridMssReductionMpfr : public task {
         }
         else{
             int newDepth = depth - 1;
-            mss_struct_interval resLeft;
-            mss_struct_interval resRight;
-            long middle = (left + right) / 2;
+            mss_struct_mpfr resLeft;
+            mss_struct_mpfr resRight;
+            unsigned long middle = (left + right) / 2;
             int lIndex = 2*index;
             int rIndex = lIndex + 1;
+            
+            mss_struct_bool resBoolLeft, resBoolRight;
+            bool t;
+
+            boolean b = memo[depth][index][3];
+            switch(b){
+                case True:
+                if(res_bool->mss){
+                    res_bool->mss = false;
+                    resBoolRight.mps = true;
+                    resBoolLeft.mts = true;
+                }
+                if(res_bool->posl){
+                    res_bool->posl = false;
+                    resBoolLeft.posMps = true;
+                }
+                if(res_bool->posr){
+                    res_bool->posr = false;
+                    resBoolRight.posMts = true;
+                }
+                break;
+                case Undefined:
+                // Variables used in the conditionnal
+                t = res_bool->mss || res_bool->posl || res_bool->posr;
+
+                // Assignments
+                if(res_bool->mss){
+                    resBoolRight.mps = true;
+                    resBoolLeft.mts = true;
+                }
+                if(res_bool->posl){
+                    resBoolLeft.posMps = true;
+                }
+                if(res_bool->posr){
+                    resBoolRight.posMts = true;
+                }
+                if(t){
+                    res_bool->mss = true;
+                    resBoolLeft.mts = true;
+                    resBoolRight.mps = true;
+                }
+                default:
+                break;
+            }
+
+            b = memo[depth][index][2];
+            switch(b){
+                case True:
+                if(res_bool->mss){
+                    res_bool->mss = false;
+                    resBoolLeft.mss = true;
+                }
+                if(res_bool->posl){
+                    res_bool->posl = false;
+                    resBoolLeft.posl = true;
+                }
+                if(res_bool->posr){
+                    res_bool->posr = false;
+                    resBoolLeft.posr = true;
+                }
+                break;
+                case False:
+                if(res_bool->mss){
+                    res_bool->mss = false;
+                    resBoolRight.mss = true;
+                }
+                if(res_bool->posl){
+                    res_bool->posl = false;
+                    resBoolRight.posl = true;
+                }
+                if(res_bool->posr){
+                    res_bool->posr = false;
+                    resBoolRight.posr = true;
+                }
+                break;
+                default:
+                // Variables used in the conditionnal
+                t = res_bool->mss || res_bool->posl || res_bool->posr;
+
+                if(res_bool->mss){
+                    res_bool->mss = false;
+                    resBoolRight.mss = true;
+                    resBoolLeft.mss = true;
+                }
+                if(res_bool->posl){
+                    res_bool->posl = false;
+                    resBoolRight.posl = true;
+                    resBoolLeft.posl = true;
+                }
+                if(res_bool->posr){
+                    res_bool->posr = false;
+                    resBoolRight.posr = true;
+                    resBoolLeft.posr = true;
+                }
+
+                if(t){
+                    resBoolLeft.mss = true;
+                    resBoolRight.mss = true;
+                }
+            }
+
+            // Mts
+            b = memo[depth][index][1];
+            switch(b){
+                case True:
+                if(res_bool->mts){
+                    res_bool->mts = false;
+                    resBoolRight.sum = true;
+                    resBoolLeft.mts = true;
+                }
+                if(res_bool->posMts){
+                    res_bool->posMts = false;
+                    resBoolLeft.posMts = true;
+                }
+                break;
+                case False:
+                if(res_bool->mts){
+                    res_bool->mts = false;
+                    resBoolRight.mts = true;
+                }
+                if(res_bool->posMts){
+                    res_bool->posMts = false;
+                    resBoolRight.posMts = true;
+                }
+                break;
+                default:
+                t = res_bool->mts || res_bool->posMts;
+
+                if(res_bool->mts){
+                    res_bool->mts = false;
+                    resBoolRight.sum = true;
+                    resBoolLeft.mts = true;
+                    resBoolRight.mts = true;
+                }
+                if(res_bool->posMts){
+                    res_bool->posMts = false;
+                    resBoolRight.posMts = true;
+                    resBoolLeft.posMts = true;
+                }
+
+                if(t){
+                    resBoolRight.sum = true;
+                    resBoolLeft.mts = true;
+                    resBoolRight.mts = true;
+                }
+            }
+
+            // Mps
+            b = memo[depth][index][0];
+            switch(b){
+                case True:
+                if(res_bool->mps){
+                    res_bool->mps = false;
+                    resBoolLeft.sum = true;
+                    resBoolRight.mps = true;
+                }
+                if(res_bool->posMps){
+                    res_bool->posMps = false;
+                    resBoolRight.posMps = true;
+                }
+                break;
+                case False:
+                if(res_bool->mps){
+                    res_bool->mps = false;
+                    resBoolLeft.mps = true;
+                }
+                if(res_bool->posMps){
+                    res_bool->posMps = false;
+                    resBoolLeft.posMps = true;
+                }
+                break;
+                default:
+                t = res_bool->mps || res_bool->posMps;
+
+                if(res_bool->mps){
+                    res_bool->mps = false;
+                    resBoolLeft.mps = true;
+                    resBoolLeft.sum = true;
+                    resBoolRight.mps = true;
+                }
+                if(res_bool->posMps){
+                    res_bool->posMps = false;
+                    resBoolLeft.posMps = true;
+                    resBoolRight.posMps = true;
+                }
+
+                if(t){
+                    resBoolLeft.mps = true;
+                    resBoolLeft.sum = true;
+                    resBoolRight.mps = true;
+                }
+
+            }
+
+            // Sum
+            if(res->sum){
+                res->sum = false;
+                resBoolLeft.sum = true;
+                resBoolRight.sum = true;
+            }
 
             // Call subtasks and return result;
-                HybridMssReductionMpfr& lTask = *new(allocate_child()) HybridMssReductionMpfr(newDepth,lIndex,a,&resLeft,left,middle,memo);
-                HybridMssReductionMpfr &rTask = *new(allocate_child()) HybridMssReductionMpfr(newDepth,rIndex,a,&resRight,middle,right,memo);
+            HybridMssReductionMpfr& lTask = *new(allocate_child()) HybridMssReductionMpfr(newDepth,lIndex,a,&resLeft,&resBoolLeft,left,middle,memo);
+            HybridMssReductionMpfr &rTask = *new(allocate_child()) HybridMssReductionMpfr(newDepth,rIndex,a,&resRight,&resBoolRight,middle,right,memo);
 
-                set_ref_count(3);
-                spawn(lTask);
-                spawn_and_wait_for_all(rTask);
+            set_ref_count(3);
+            spawn(lTask);
+            spawn_and_wait_for_all(rTask);
 
-                // Join
-                boolean b;
-                // Sum
-                res->sum = in2_add(resLeft.sum,resRight.sum);
+            // Join
 
-                // Mps
-                __m128d mpsAux = in2_add(resLeft.sum,resRight.mps);
-                b = in2_ge(mpsAux,resLeft.mps);
-                switch(b){
-                    case True:
-                    res -> mps = mpsAux;
-                    res -> posMps = resRight.posMps;
-                    break;
-                    case False:
-                    res -> mps = resLeft.mps;
-                    res -> posMps = resLeft.posMps;
-                    break;
-                    default:
-                    res -> mps = in2_merge(mpsAux,resLeft.mps);
-                }
-                memo[depth][index][0] = b;
 
-                // Mts
-                __m128d mtsAux = in2_add(resRight.sum,resLeft.mts);
-                b = in2_ge(mtsAux,resRight.mts);
-                switch(b){
-                    case True:
-                    res -> mts = mtsAux;
-                    res -> posMts = resLeft.posMts;
-                    break;
-                    case False:
-                    res->mts = resRight.mts;
-                    res->posMts = resRight.posMts;
-                    break;
-                    default:
-                    res->mts = in2_merge(mtsAux,resRight.mts);
-                }
-                memo[depth][index][1] = b;
 
-                // Mss
-                b = in2_ge(resLeft.mss,resRight.mss);
-                switch(b){
-                    case True:
-                    res->mss = resLeft.mss; 
-                    res->posl = resLeft.posl;
-                    res->posr = resLeft.posr;
-                    break;
-                    case False:
-                    res->mss = resRight.mss; 
-                    res->posl = resRight.posl;
-                    res->posr = resRight.posr;
-                    break;
-                    default:
-                    res->mss = in2_merge(resLeft.mss,resRight.mss);
-                }
-                memo[depth][index][2] = b;
 
-                __m128d mssAux = in2_add(resRight.mps,resLeft.mts);
-                b = in2_ge(mssAux,res->mss);
-                switch(b){
-                    case True:
-                    res->mss = mssAux;
-                    res->posl = resLeft.posMts;
-                    res->posr = resRight.posMps;
-                    break;
-                    case Undefined:
-                    res->mss = in2_merge(res->mss,mssAux);
-                    break;
-                    default:
-                    break;
-                }
-                memo[depth][index][3] = b;
 
         }
         return NULL;
